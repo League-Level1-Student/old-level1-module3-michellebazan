9ecff0f606031ca83cb49311892644d1e8b62f6a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-04-17 16:54:12 -0700

    WackAMole(WIP)

--- src/extra/WackAMole.java
+++ src/extra/WackAMole.java
@@ -1,82 +1,119 @@
 package extra;
 
+
+import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class WackAMole {
+public class WackAMole implements ActionListener {
     
     public static void main(String[] args) {
     
     WackAMole mole = new WackAMole();
     mole.createUI();
-    mole.drawButtons();
+    
+    Random rand = new Random();
+    int a = rand.nextInt(24);
+    mole.drawButtons(a);
+    
     
     }
     
-    JButton b1 = new JButton();
-    JButton b2 = new JButton();
-    JButton b3 = new JButton();
-    JButton b4 = new JButton();
-    JButton b5 = new JButton();
-    JButton b6 = new JButton();
-    JButton b7 = new JButton();
-    JButton b8 = new JButton();
-    JButton b9 = new JButton();
-    JButton b10 = new JButton();
-    JButton b11 = new JButton();
-    JButton b12 = new JButton();
-    JButton b13 = new JButton();
-    JButton b14 = new JButton();
-    JButton b15 = new JButton();
-    JButton b16 = new JButton();
-    JButton b17 = new JButton();
-    JButton b18 = new JButton();
-    JButton b19 = new JButton();
-    JButton b20 = new JButton();
-    JButton b21 = new JButton();
-    JButton b22 = new JButton();
-    JButton b23 = new JButton();
-    JButton b24 = new JButton();
     
+    Date date;
+      JPanel p = new JPanel();
+ JFrame j = new JFrame("Wack a Button");
+        
+ 
     public void createUI () {
-        JFrame j = new JFrame("Wack a Button");
-        JPanel p = new JPanel();
         j.add(p);
         j.setVisible(true);
         p.setVisible(true);
-        j.setSize(120, 125);
-        
-        p.add(b1);
-        p.add(b2);
-        p.add(b3);
-        p.add(b4);
-        p.add(b5);
-        p.add(b6);
-        p.add(b7);
-        p.add(b8);
-        p.add(b9);
-        p.add(b10);
-        p.add(b11);
-        p.add(b12);
-        p.add(b13);
-        p.add(b14);
-        p.add(b15);
-        p.add(b16);
-        p.add(b17);
-        p.add(b18);
-        p.add(b19);
-        p.add(b20);
-        p.add(b21);
-        p.add(b22);
-        p.add(b23);
-        p.add(b24);
+        j.setSize(800, 600);    
     }
     
-    public void drawButtons() {
-    	Random rand = new Random();
-    	//http://level1.jointheleague.org/Mod4Recipes/WhackAMole.html
+    
+    JButton b;
+    
+    
+    public void drawButtons(int number) {
+    	date = new Date();
+    	for(int i = 0; i < 24; i++) {
+    	b = new JButton();
+    	b.setSize(40,40);
+    	b.setText("    ");
+    	b.addActionListener(this);
+    	if (i==number) {
+    		b.setText("mole");
+    	}
+    	p.add(b);
+       }
     }
+    
+    
+    void speak(String words) {
+        try {
+             Runtime.getRuntime().exec("say " + words).waitFor();
+        } catch (Exception e) {
+             e.printStackTrace();
+        }
+   }
+    
+    
+    int count = 0;
+    int countMissed = 0;
+    
+    
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton buttonPressed = (JButton)e.getSource();
+		if(buttonPressed.getText().equals("    ")) {
+			countMissed++;
+			//Count how many times they hit something other than the mole! button and change the "missed" comments each time (be nice)
+			speak("Be nice");
+			j.dispose();
+			Random rand = new Random();
+		    int a = rand.nextInt(24);
+			drawButtons(a);
+		} else {
+			count++;
+			
+//			Random rand = new Random();
+//		    int a = rand.nextInt(24);
+//			drawButtons(a);
+		}
+		
+		if(count==10) {
+			endGame(date,count);
+		}
+		
+		if(countMissed==5) {
+			speak("You lost");
+			endGame(date,countMissed);
+		}
+		
+	}
+	
+	
+	private void endGame(Date timeAtStart, int molesWhacked) {
+	     Date timeAtEnd = new Date();
+	     JOptionPane.showMessageDialog(null, "Your whack rate is "
+	          + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+	          + " moles per second.");
+	}
+	
+	
+	private void playSound(String fileName) {
+	     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+	     sound.play();
+	}
+    //http://level1.jointheleague.org/Mod4Recipes/WhackAMole.html
 }
