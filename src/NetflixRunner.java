5d86f4f8709978d3e6d71868c0c69dee23105ba5
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-06-05 16:58:59 -0700

    NetflixRunner

--- src/NetflixRunner.java
+++ src/NetflixRunner.java
@@ -1,4 +1,13 @@
 
 public class NetflixRunner {
+public static void main(String[] args) {
+Movie m1 = new Movie("0.0",2);
+Movie m2 = new Movie("Twilight",1);
+Movie m3 = new Movie("Coco",5);
+Movie m4 = new Movie("Silent Voice",4);
+Movie m5 = new Movie("Your Name",4);
+m1.getTicketPrice();
+
+}
 
 }
