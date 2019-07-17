a8cbaaf5a132f6c359c9c37ef6b9e7ad4a78be6a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-07-17 16:57:48 -0700

    checkpointThing

---
+++ src/extra/CheckpointThing.java
@@ -0,0 +1,65 @@
+package extra;
+
+public class CheckpointThing {
+//	 1.Name 5 JComponents used in the following GUI:
+//
+//
+// button
+//         _________________________________________________
+//
+// text field
+//         _________________________________________________
+//
+//frame
+//         _________________________________________________
+//
+//label
+//         _________________________________________________
+//
+//panel
+//         _________________________________________________
+//
+//2. What would be the best type of listener you could use, to tell when…
+//
+//
+//      The mouse was moved    mouse motion listener_________________________________________________
+//
+//
+//      A button was clicked   action listener_________________________________________________
+//
+//
+//      An image was clicked   _________________________________________________
+//
+//
+//      Any key was pressed   _________________________________________________
+//
+//
+//3. Given the listeners that you put in the answers above, what is the name of the method that will be called when...
+//
+//
+//      A letter is typed?   _________________________________________________
+//
+//
+//      A button is clicked?_________________________________________________
+//
+//
+//      A mouse is clicked?_________________________________________________
+//
+//
+//
+//4. Write code to add listeners to the following components:
+//
+//
+//      JButton amazingButton = new JButton();
+//
+//
+//      _________________________________________________
+//
+//
+//      JFrame myFrame = new JFrame();
+//
+//
+//      _________________________________________________
+//
+
+}
